8b71d223deda462a50734ae4b3ad1e17f37a1f9d
Matt Freeman <mgregfreeman@gmail.com>
2023-01-14 09:01:34 -0700

    update logo to tighter svg

--- stat_explorer/src/components/Layout/Header.js
+++ stat_explorer/src/components/Layout/Header.js
@@ -4,7 +4,7 @@ import Logo from '../../assets/HoopsLogo';
 export default function ({options, onOptionClick}) {
   return (
     <header className="flex-aic px-m header">
-      <Logo className="header__logo" size="40px" fill="#003fa2" />
+      <Logo className="header__logo" size="38px" fill="#003fa2" />
       <span className='header__title ml-s'>BYU HOOPS STATS</span>
     </header>
   );
